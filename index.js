b735d02284751a047b5141071f6f39759094e03e
sagorhasan17 <sagorhasan.info80@gmail.com>
2026-05-20 21:28:05 +0600

    server setup for tutorly project and nessesary thing insatll

---
+++ index.js
@@ -0,0 +1,48 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function server() {
+  try {
+    await client.connect();
+    const db = client.db("tutorlyDB");
+    const teacherCollection = db.collection("teachers");
+    const bookingsCollection = db.collection("bookings");
+
+
+
+
+    await client.db("tutorlyDB").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+server().catch(console.dir);
+
+app.listen(PORT, () => {
+    console.log(`This Server is Running on Port ${PORT}`);
+});
