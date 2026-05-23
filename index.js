e39b2a8999fb02242707ba9d8d5fdc80c4168deb
sagorhasan17 <sagorhasan.info80@gmail.com>
2026-05-23 16:16:25 +0600

    deploy

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const uri = process.env.MONGODB_URI;
 const app = express();
 const PORT = process.env.PORT || 5000;
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
